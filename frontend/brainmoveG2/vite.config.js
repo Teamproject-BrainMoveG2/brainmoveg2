36ab2649bf75f8b4e641c5f4ee02be6eb04ee76f
MaartenSchaep <maarten.schaep@student.howest.be>
2026-01-22 10:37:04 +0100

    feat: added imports to vite config

--- frontend/brainmoveG2/vite.config.js
+++ frontend/brainmoveG2/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  build: {
+    rollupOptions: {
+      external: [
+        'vuetify/styles',
+        '@mdi/font/css/materialdesignicons.css',
+        'vuetify',
+        'vuetify/iconsets/mdi',
+        'vuetify/components',
+      ],
+    },
+  },
+});
